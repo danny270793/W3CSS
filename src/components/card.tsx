5f254691e1b542bc9a5f46fc6f3583179c35ebb6
Danny Vaca <dvaca@technisys.com>
2025-07-01 13:22:55 -0500

    add default elevation

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,8 +2,13 @@ import type { FC, ReactNode } from "react";
 
 interface CardProps {
   children?: ReactNode;
+  elevation?: 2 | 4;
 }
 
 export const Card: FC<CardProps> = (props: CardProps): ReactNode => {
-  return <div className="w3-card">{props.children}</div>;
+  return (
+    <div className={`w3-card-${props.elevation ? props.elevation : 2}`}>
+      {props.children}
+    </div>
+  );
 };
