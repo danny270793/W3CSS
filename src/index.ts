c730ad7892d61f5dd007a5e6439d8468cc1ad0d5
Danny Vaca <dvaca@technisys.com>
2025-07-02 12:05:53 -0500

    expose display

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@ export { Container } from "./components/container"
 export { Panel } from "./components/panel"
 export { Card } from "./components/card"
 export { Button } from "./components/button"
+export {
+  Display,
+  DisplayBottomLeft,
+  DisplayBottomMiddle,
+  DisplayBottomRight,
+  DisplayLeft,
+  DisplayMiddle,
+  DisplayRight,
+  DisplayTopLeft,
+  DisplayTopMiddle,
+  DisplayTopRight,
+} from "./components/display"
