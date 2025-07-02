a53bdc3cd6f0a5cf4d587080d04d52475af68ae0
Danny Vaca <dvaca@technisys.com>
2025-07-02 17:41:03 -0500

    dropdown element

---
+++ src/components/dropdown.tsx
@@ -0,0 +1,24 @@
+import type { FC, ReactNode } from "react"
+import { Element, type DisplayProps } from "./element"
+import { Button } from "./button"
+
+interface DropdownProps extends DisplayProps {
+  label: string
+}
+
+export const Dropdown: FC<DropdownProps> = (
+  props: DropdownProps,
+): ReactNode => {
+  return (
+    <Element
+      {...props}
+      style={props.style}
+      className={[`w3-dropdown-hover`, props.className].join(" ")}
+    >
+      <Button className="w3-button">{props.label}</Button>
+      <Element className="w3-dropdown-content w3-bar-block" border>
+        {props.children}
+      </Element>
+    </Element>
+  )
+}
