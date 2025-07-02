4079ab44b506b15e7e1b369128b7dcd5e0249867
Danny Vaca <dvaca@technisys.com>
2025-07-01 21:24:12 -0500

    allow to have borders

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,21 +1,25 @@
 import type { FC, ReactNode } from "react"
 import type { Color } from "./colors"
 import { Div } from "./div"
+import type { Border } from "./border"
 
 interface CardProps {
   children?: ReactNode
   elevation?: 2 | 4
   color?: Color
   className?: string
+  border?: boolean | Border
 }
 
 export const Card: FC<CardProps> = (props: CardProps): ReactNode => {
   return (
     <Div
       color={props.color}
-      className={`w3-card-${props.elevation ? props.elevation : 2} ${
-        props.className
-      }`}
+      border={props.border}
+      className={[
+        `w3-card-${props.elevation ? props.elevation : 2}`,
+        props.className,
+      ].join(" ")}
     >
       {props.children}
     </Div>
