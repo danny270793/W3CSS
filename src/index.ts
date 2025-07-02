c102686ccaebbb225c2dcfe2d8f73ffd9e41a267
Danny Vaca <dvaca@technisys.com>
2025-07-02 08:34:25 -0500

    export rest elements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "./styles/w3css.css"
 
-export { Card } from "./components/card"
+export { Element } from "./components/element"
 export { Container } from "./components/container"
 export { Panel } from "./components/panel"
+export { Card } from "./components/card"
