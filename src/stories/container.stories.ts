b3858727271829443d44652b63994ea93fa55881
Danny Vaca <dvaca@technisys.com>
2025-07-01 21:12:34 -0500

    create container story

---
+++ src/stories/container.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react-vite"
+
+import { Container } from "../components/container"
+
+const meta = {
+  title: "Container",
+  component: Container,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Container>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Normal: Story = {
+  args: {
+    children: "This is a normal container",
+  },
+}
+
+export const BlueNormal: Story = {
+  args: {
+    children: "This is a normal blue container",
+    color: "blue",
+  },
+}
