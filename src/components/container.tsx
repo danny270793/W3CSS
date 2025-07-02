f0424c5396c31c00e0d8b5c8892d7a4d3d792ce6
Danny Vaca <dvaca@technisys.com>
2025-07-01 21:24:19 -0500

    allow to have borders

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,18 +1,24 @@
 import type { FC, ReactNode } from "react"
 import type { Color } from "./colors"
 import { Div } from "./div"
+import type { Border } from "./border"
 
 interface ContainerProps {
   children?: ReactNode
   color?: Color
   className?: string
+  border?: boolean | Border
 }
 
 export const Container: FC<ContainerProps> = (
   props: ContainerProps,
 ): ReactNode => {
   return (
-    <Div color={props.color} className={`w3-container ${props.className}`}>
+    <Div
+      color={props.color}
+      className={["w3-container", props.className].join(" ")}
+      border={props.border}
+    >
       {props.children}
     </Div>
   )
