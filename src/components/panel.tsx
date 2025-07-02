15742374befb8e2dbaa502ec77249d08a9654ecf
Danny Vaca <dvaca@technisys.com>
2025-07-01 21:24:30 -0500

    allow to have bordes

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -1,16 +1,22 @@
 import type { FC, ReactNode } from "react"
 import type { Color } from "./colors"
 import { Div } from "./div"
+import type { Border } from "./border"
 
 interface PanelProps {
   children?: ReactNode
   color?: Color
   className?: string
+  border?: boolean | Border
 }
 
 export const Panel: FC<PanelProps> = (props: PanelProps): ReactNode => {
   return (
-    <Div color={props.color} className={`w3-panel ${props.className}`}>
+    <Div
+      color={props.color}
+      border={props.border}
+      className={["w3-panel", props.className].join(" ")}
+    >
       {props.children}
     </Div>
   )
