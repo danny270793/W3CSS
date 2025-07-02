1fd630dc114c807196ae08160cb952f84d15f50c
Danny Vaca <dvaca@technisys.com>
2025-07-02 09:23:01 -0500

    improve card story

--- src/stories/card.stories.tsx
+++ src/stories/card.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react-vite"
 
 import { Card } from "../components/card"
 import { Container } from "../components/container"
+import { Button } from "../components/button"
 
 const meta = {
   title: "Card",
@@ -72,9 +73,44 @@ export const CallCard: Story = {
           style={{ width: "80%" }}
         />
         <h5>John Doe</h5>
+        <div className="w3-section">
+          <Button color="green">Accept</Button>
+          <Button color="red">Decline</Button>
+        </div>
       </Container>
     ),
     color: "dark-gray",
+    elevation: 4,
+  },
+}
+
+export const EmailCard: Story = {
+  args: {
+    children: (
+      <>
+        <Container color="light-gray">
+          <h3>John Doe</h3>
+        </Container>
+        <Container>
+          <p>1 new friend request</p>
+          <hr />
+          <img
+            src="https://www.w3schools.com/w3css/img_avatar3.png"
+            alt="Avatar"
+            className="w3-left w3-circle w3-margin-right"
+            style={{ width: "60px" }}
+          />
+          <p>
+            CEO at Mighty Schools. Marketing and Advertising. Seeking a new job
+            and new opportunities.
+          </p>
+          <br />
+        </Container>
+        <Button color="dark-gray" block>
+          + Connect
+        </Button>
+      </>
+    ),
     elevation: 4,
   },
 }
